50bb58ed65142b0f970432b2471f57d7fcdafaf7
Nicholas <nicholas_hagan@sfu.ca>
2020-07-20 23:26:21 -0700

    changed google client id and jwt secret to hidden variables

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const baseURL = 'http://museical.herokuapp.com' // needs to be changed to museic
 
 module.exports = {
     // The secret for the encryption of the jsonwebtoken
-    JWTsecret: 'mysecret',
+    JWTsecret: process.env.GOOGLE_CLIENT_ID,
     baseURL: baseURL,
     port: 80,
     // The credentials and information for OAuth2
     oauth2Credentials: {
-      client_id: "603204963619-jklvtgamuv2u4gjleafavj63cojk9jsj.apps.googleusercontent.com",
+      client_id: process.env.GOOGLE_CLIENT_ID,
       project_id: "museical", // The name of your project
       auth_uri: "https://accounts.google.com/o/oauth2/auth",
       token_uri: "https://oauth2.googleapis.com/token",
