77203235974c48245a8357b83256d957ab00d875
Divyansh Khari <divyanshkhari@DivyanshMBP.hitronhub.home>
2020-07-23 02:53:14 -0700

    Project

--- index.js
+++ index.js
@@ -10,32 +10,7 @@ const googleConfig = {
   clientSecret: 'FzuBw_NZ1_WS_7vrLcJvfJj9',
   redirect: 'https://museical.herokuapp.com'
 };
-const queryString = require('query-string');
 
-//console.log(location.search);
-//=> '?foo=bar'
-
-const parsed = queryString.parse(location.search);
-console.log(parsed);
-//=> {foo: 'bar'}
-
-console.log(location.hash);
-//=> '#token=bada55cafe'
-
-const parsedHash = queryString.parse(location.hash);
-console.log(parsedHash);
-//=> {token: 'bada55cafe'}
-
-parsed.foo = 'unicorn';
-parsed.ilike = 'pizza';
-
-const stringified = queryString.stringify(parsed);
-//=> 'foo=unicorn&ilike=pizza'
-
-location.search = stringified;
-// note that `location.search` automatically prepends a question mark
-console.log(location.search);
-//=> '?foo=unicorn&ilike=pizza'
 const fs = require('fs')
 
 const google = require('googleapis').google;
