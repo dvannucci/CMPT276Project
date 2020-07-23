3d029c0dff59ba1d40d6fc2de5cea721fa80a2c8
Divyansh Khari <divyanshkhari@DivyanshMBP.hitronhub.home>
2020-07-23 02:33:12 -0700

    Project

--- index.js
+++ index.js
@@ -66,31 +66,6 @@ function getGooglePlusApi(auth) {
   return google.plus({ version: 'v1', auth });
 }
 
-function getGoogleAccountFromCode(code) {
-  const data = await auth.getToken(code);
-  const tokens = data.tokens;
-  /*const auth = createConnection();
-  auth.setCredentials(tokens);
-  const plus = getGooglePlusApi(auth);
-  const me = await plus.people.get({ userId: 'me' });
-  const userGoogleId = me.data.id;
-  const userGoogleEmail = me.data.emails && me.data.emails.length && me.data.emails[0].value;
-  return {
-    id: userGoogleId,
-    email: userGoogleEmail,
-    tokens: tokens,
-  };
-  */
-}
-
-
-
-
-
-
-
-
-
 
 // Storgae destination for profile pictures, and the name of the picture.
 const storage = multer.diskStorage({
