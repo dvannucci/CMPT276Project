da58ee0b25d7031e169fe78552feedbe0e4a8705
Divyansh Khari <divyanshkhari@DivyanshMBP.hitronhub.home>
2020-07-23 01:46:10 -0700

    Getting URL which will open google sign in

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ const defaultScope = [
   'https://www.googleapis.com/auth/userinfo.email',
 ];
 
+/**
+ * Get a url which will open the google sign-in page and request access to the scope provided
+ */
+function getConnectionUrl(auth) {
+  return auth.generateAuthUrl({
+    access_type: 'offline',
+    prompt: 'consent',
+    scope: defaultScope
+  });
+}
+
 
 
 
