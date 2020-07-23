99ae9f60737193412fdce3994494be07268de78f
Divyansh Khari <divyanshkhari@DivyanshMBP.hitronhub.home>
2020-07-23 02:11:14 -0700

    Project

--- index.js
+++ index.js
@@ -66,6 +66,21 @@ function getGooglePlusApi(auth) {
   return google.plus({ version: 'v1', auth });
 }
 
+function getGoogleAccountFromCode(code) {
+  const data = await auth.getToken(code);
+  const tokens = data.tokens;
+  const auth = createConnection();
+  auth.setCredentials(tokens);
+  const plus = getGooglePlusApi(auth);
+  const me = await plus.people.get({ userId: 'me' });
+  const userGoogleId = me.data.id;
+  const userGoogleEmail = me.data.emails && me.data.emails.length && me.data.emails[0].value;
+  return {
+    id: userGoogleId,
+    email: userGoogleEmail,
+    tokens: tokens,
+  };
+}
 
 
 
