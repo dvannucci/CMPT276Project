d24a6810cf219dda5b0cb1c152f9aaf5e7b981f8
Nicholas <nicholas_hagan@sfu.ca>
2020-07-23 20:25:16 -0700

    removed nonfunctional socket.io test

--- test/server-test.js
+++ test/server-test.js
@@ -9,10 +9,6 @@ chai.use(chaiHttp);
 
 const userCredentials = {username:'john', mypassword:'guest'}
 
-
-
-
-
 var authenticatedUser = chai.request.agent(app);
 before(function(done){
   authenticatedUser
@@ -27,7 +23,7 @@ before(function(done){
 
 describe('GET /chat/1', function(done){
     //addresses 2nd bullet point: if the user is not logged in we should get a 302 response code and be directed to the /login page
-    it('should return a 302 response and redirect to /login', function(done){
+    it('should return a 302 response and redirect to /', function(done){
         chai.request(app).get('/chat/0')
         .end(function(error,res){
             expect(res).to.have.status(200);
@@ -43,15 +39,24 @@ describe('GET /chat/1', function(done){
             done();
         });
       });
+    });
 
-      it('must connect', function (done) {
-        server.listen(function (addr, server) {
-          var cl = client(addr);
-          cl.on('open', function () {
-            cl.close();
-            server.close();
-            done();
+    describe('GET /videos', function(done){
+      //addresses 2nd bullet point: if the user is not logged in we should get a 302 response code and be directed to the /login page
+      it('should return a 302 response and redirect to /login', function(done){
+          chai.request(app).get('/chat/0')
+          .end(function(error,res){
+              res.should.have.status(200)
+              expect(res).to.have.header('location', '/');
+              done();
+          });
+        });
+      //addresses 1st bullet point: if the user is logged in we should get a 200 status code
+      it('should return a 200 response if the user is logged in', function(done){
+          authenticatedUser.get('/chat/1')
+          .end(function(error,res){
+              res.should.have.status(200);
+              done();
           });
         });
       });
-    });
