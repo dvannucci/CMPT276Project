092eb6e45609cd4df002615fa3f45042f5de64b4
Divyansh Khari <divyanshkhari@DivyanshMBP.hitronhub.home>
2020-07-29 07:01:11 -0700

    Project

--- index.js
+++ index.js
@@ -461,6 +461,10 @@ app.post('/userInfoUpdate', checkLogin, async (req, res) => {
       return  res.render('pages/mymusic', { 'username' : req.session.username, 'id' : req.session.loggedID, likedVids: response[0].data.items });
     });
   });
+  app.get('/maps',(req,res)=>{
+
+    return res.render('/Maps');
+  });
 
   app.get('/videos', checkLogin, (req, res) => {
     if (!req.cookies.jwt) {
